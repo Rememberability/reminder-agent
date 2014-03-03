7adfb6a97073e0facea49eef1b57c058519e2961
Ravi Desai <ravi@ravidesai.com>
2014-03-02 19:36:57 -0500

    Bypass CanCan issue 835
    - Perhaps consider using Pundit for authorization?
    -- Doesn't seem to keep everything in one file, though.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,19 @@
 class ItemsController < ApplicationController
   before_filter :find_item, only: [:show, :remember, :forget, :edit]
-  load_and_authorize_resource
 
   def index
     @items = Item.all
   end
 
   def new
+    authorize! :new, Item
     @item = Item.new
   end
 
   def create
     @item = Item.new(whitelisted_params)
     @item.user = current_user
+    authorize! :create, @item
     if @item.save
       flash[:now] = "Saved a new item to remember."
       redirect_to [@user, @item]
@@ -27,6 +28,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.new(whitelisted_params)
+    authorize! :edit, @item
     if @item.save
       redirect_to @item
     else
@@ -36,11 +38,13 @@ class ItemsController < ApplicationController
   end
 
   def remember
+    authorize! :remember, @item
     @item.remember
     redirect_to current_user
   end
 
   def forget
+    authorize! :forget, @item
     @item.forget
     redirect_to current_user
   end
